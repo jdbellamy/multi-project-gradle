d68a28a4ed02a78554c680bb99f4804fe6ecc38b
jdbellamy <jdbellamy@me.com>
2016-05-23 19:53:26 -0400

    Initial add

---
+++ left/src/main/java/com/example/Main.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class Main {
+
+    public String check() {
+        return "left.Main.check()";
+    }
+}
---
+++ src/main/groovy/com/example/MultiProjectGradleApplication.groovy
@@ -0,0 +1,12 @@
+package com.example
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class MultiProjectGradleApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run MultiProjectGradleApplication, args
+	}
+}
